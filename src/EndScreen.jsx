736a84cb21bfddc2c4a1ac701c2bb6735ff34c79
ShaynaMBarclay <shaynambarclay@gmail.com>
2025-08-23 11:36:02 -0400

    game made

--- src/EndScreen.jsx
+++ src/EndScreen.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import wow from "./assets/wow.jpeg";
+import italy from "./assets/italy.jpeg";
+import france from "./assets/france.jpeg";
 
 export default function EndScreen() {
   return (
     <div className="end-screen">
-      <h1>🎉 Level 3 Complete 🎉</h1>
-      <p>Forever unlocked ❤️</p>
+      <h1>🎉 3 years, Complete! 🎉</h1>
+      <p>Forever, in every universe, unlocked ❤️</p>
       <div className="collage">
-        <img src="yourPic1.jpg" alt="memory" />
-        <img src="yourPic2.jpg" alt="memory" />
-        <img src="yourPic3.jpg" alt="memory" />
+        <img src={wow} alt="memory 1" />
+        <img src={italy} alt="memory 2" />
+        <img src={france} alt="memory 3" />
       </div>
       <Link to="/">
         <button className="restart-btn">🏠 Back to Start</button>
